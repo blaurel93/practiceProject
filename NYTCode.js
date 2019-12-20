7b1f6505268083908111f30e437b7d4fbf8883e8
Blaise <ifollowjre@gmail.com>
2019-12-19 21:27:11 -0700

    need to finish this

--- NYTCode.js
+++ NYTCode.js
@@ -8,26 +8,44 @@ function clear() {
 function buildQueryUrl() {
 
   var search = $.trim($("#search-term").val());
-  var startYear = $("#start-year").val();
-  if (parseInt(startYear)) {
-    startYear = startYear + '0101';
-  }
-  var endYear = $("#end-year").val();
-  if (parseInt(endYear)) {
-    endYear = endYear + '1231';
-  }
+  var startYear = '&begin_date=' + $("#start-year").val() + '0101';
+
+  var endYear = '&end_date=' + $("#end-year").val() + '1231';
+
   var myUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"
-  var queryURL = myUrl + "q=" + search + "&api-key=olfw5PJPxiPCBXdeZLuR47neRxgOj6ga" + "&begin_date=" + startYear + "&end_date=" + endYear;
+  var queryURL = myUrl + "q=" + search + "&api-key=olfw5PJPxiPCBXdeZLuR47neRxgOj6ga" + startYear + endYear;
 
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function (res) {
-    console.log(res.response.docs[0]);
-    console.log(queryURL);
-  });
-  console.log(queryURL);
+  }).then(updatePage);
+
 }
+
+function updatePage(NYTData) {
+  var numArticles = $("#article-count").val();
+
+  for (var = i; i < nydata.response.docs.length; i++) {
+    var article = NYTData.response.docs[i];
+
+    var articleCount = i + 1;
+
+    var $articleList = $("<ul>");
+    $articleList.addClass("list-group");
+
+    $("article-section").append($articleList);
+
+    var headline = article.headline;
+    var $articleListItem = $("<li class='list-group-item articleHeadline'>");
+
+    if (headline &&)
+  }
+
+}
+
+
+
+
 
 
 $("#run-search").on("click", function () {
