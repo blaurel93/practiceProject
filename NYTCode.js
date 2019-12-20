2ad6b7685c93026fa1a3df4e2f6ec69e897cbdda
Blaise <ifollowjre@gmail.com>
2019-12-19 20:45:45 -0700

    adding query url function

--- NYTCode.js
+++ NYTCode.js
@@ -1,27 +1,33 @@
-var search = $("#search-term").val();
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=olfw5PJPxiPCBXdeZLuR47neRxgOj6ga&page=0";
 
-// $.ajax({
-//   url: queryURL,
-//   method: "GET"
-// }).then(function (res) {
-//   console.log(res.response.docs[0]);
-//   console.log(res.response.docs[1]);
-//   console.log(res.response.docs[2]);
-//   console.log(res.response.docs[3]);
-//   console.log(res.response.docs[4]);
-//   console.log(res.response.docs[5]);
-//   console.log(res.response.docs[6]);
-//   console.log(res.response.docs[7]);
-//   console.log(res.response.docs[8]);
-//   console.log(res.response.docs[9]);
-// });
 
-$("run-search").on("click", function() {
+
+function clear() {
+  $(".article-section").empty();
+}
+
+function buildQueryUrl() {
+
+  var search = $.trim($("#search-term").val());
+  var startYear = $("#start-year").val();
+
+  var endYear = $("#end-year").val();
+  var myUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"
+  var queryURL = myUrl + "q=" + search + "&api-key=olfw5PJPxiPCBXdeZLuR47neRxgOj6ga&" + "begin_date=" + startYear + "0101&end_date=" + endYear + "1212";
+
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (res) {
-    console.log(res);
+    console.log(res.response.docs[0]);
+    console.log(queryURL);
   });
+  console.log(queryURL);
+}
+
+
+$("#run-search").on("click", function () {
+  event.preventDefault();
+  clear();
+  buildQueryUrl();
+
 })
