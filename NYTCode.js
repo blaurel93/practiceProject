640d8e75d065ee9afaabe3f571ac01338d147624
Blaise <ifollowjre@gmail.com>
2019-12-19 19:45:35 -0700

    created ajax call in run search

--- NYTCode.js
+++ NYTCode.js
@@ -1,8 +1,27 @@
-var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=trump&api-key=olfw5PJPxiPCBXdeZLuR47neRxgOj6ga&page=0";
+var search = $("#search-term").val();
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=olfw5PJPxiPCBXdeZLuR47neRxgOj6ga&page=0";
 
-$.ajax({
+// $.ajax({
+//   url: queryURL,
+//   method: "GET"
+// }).then(function (res) {
+//   console.log(res.response.docs[0]);
+//   console.log(res.response.docs[1]);
+//   console.log(res.response.docs[2]);
+//   console.log(res.response.docs[3]);
+//   console.log(res.response.docs[4]);
+//   console.log(res.response.docs[5]);
+//   console.log(res.response.docs[6]);
+//   console.log(res.response.docs[7]);
+//   console.log(res.response.docs[8]);
+//   console.log(res.response.docs[9]);
+// });
+
+$("run-search").on("click", function() {
+  $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (res) {
-    console.log(res)
-  })
+    console.log(res);
+  });
+})
